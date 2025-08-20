789c09348dfd617a4e43b2de3d5b683cbae48ea4
GosGuru <mporcile59@gmail.com>
2025-08-20 20:39:58 -0300

    Corregidas rutas CSS, validacion de password y mejoras responsive

--- js/loginValidations.js
+++ js/loginValidations.js
@@ -1,30 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("loginForm");
-  const input = document.getElementById("contacto");
-  const label = document.getElementById("contactoLabel");
-  const errorMsg = document.getElementById("error");
+const form = document.getElementById("loginForm");
+const emailInput = document.getElementById("contacto");
+const passwordInput = document.getElementById("password");
+const emailLabel = document.getElementById("contactoLabel");
+const passwordLabel = document.getElementById("passwordLabel");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  // Prevenir el comportamiento por defecto del formulario
+  e.preventDefault();
 
-    // Reset de estilos
-    input.classList.remove("error-input");
-    label.classList.remove("error-label");
-    errorMsg.style.display = "none";
-    errorMsg.textContent = "";
+  // Limpiar mensajes de error
+  emailInput.classList.remove("error-input");
+  passwordInput.classList.remove("error-input");
+  emailLabel.classList.remove("error-label");
+  passwordLabel.classList.remove("error-label");
+  emailError.style.display = "none";
+  passwordError.style.display = "none";
 
-    // Validación --> solo si está vacío
-    if (input.value.trim() === "") {
-      input.classList.add("error-input");
-      label.classList.add("error-label");
-      errorMsg.textContent = "Campo vacío";
-      errorMsg.style.display = "block";
-      return;
-    }
+  // Variable para controlar errores
+  let esError = false;
 
-    // Si el campo no está vacio --> Alert "bienvenido" + Redireccionamiento
+  // Validar email
+  if (emailInput.value.trim() === "") {
+    emailInput.classList.add("error-input");
+    emailLabel.classList.add("error-label");
+    emailError.textContent = "Campo vacío";
+    emailError.style.display = "block";
+    esError = true;
+  }
 
-    alert("Bienvenid@, Redirigiendo...");
-    window.location.href = "../categories.html";
-  });
+  // Validar contraseña
+  if (passwordInput.value.trim() === "") {
+    passwordInput.classList.add("error-input");
+    passwordLabel.classList.add("error-label");
+    passwordError.textContent = "Campo vacío";
+    passwordError.style.display = "block";
+    hasError = true;
+  }
+
+  // Si hay errores, no enviar el formulario --> para que no lo envíe si está vacio... Da positivo si los dos campos están con informacion
+  if (esError) return;
+
+  alert("Bienvenid@, Redirigiendo...");
+  window.location.href = "index.html";
 });
